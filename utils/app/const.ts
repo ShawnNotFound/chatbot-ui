8e0a1d968cebb47d1976ca6a7f38a1c0738f0d09
Shawn <97796289+ShawnNotFound@users.noreply.github.com>
2024-03-14 08:45:11 +0800

    Update const.ts

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_SYSTEM_PROMPT =
-  process.env.DEFAULT_SYSTEM_PROMPT || "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
+  process.env.DEFAULT_SYSTEM_PROMPT || "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown. 用$包住不需要换行的latex公式块，用$$包住需要换行的latex公式块";
 
 export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
