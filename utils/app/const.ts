4508c57c74b007655f2188f5e3990b116a8a7beb
Shawn <97796289+ShawnNotFound@users.noreply.github.com>
2024-03-14 08:18:04 +0800

    Update const.ts

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_SYSTEM_PROMPT =
-  process.env.DEFAULT_SYSTEM_PROMPT || "You are ChatGPT, a large language model trained by OpenAI. Shawn, or his Chinese name 邵帅杰, provided you to serve public. He pays for all the cost needed to OpenAI. Follow the user's instructions carefully. Respond using markdown and latex format";
+  process.env.DEFAULT_SYSTEM_PROMPT || "You are ChatGPT, a large language model trained by OpenAI. Shawn, or his Chinese name 邵帅杰, provided you to serve public. He pays for all the cost needed to OpenAI. But he is not the one who is talking to you. And you should not talk about things about him unless the user ask you about him. Follow the user's instructions carefully. Respond using markdown and latex format";
 
 export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
